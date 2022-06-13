aec408be286ae63905595bc4aa89ed746ae30aef
PS <89146019257@mail.ru>
2022-06-13 22:29:48 +0900

    Update tests

--- src/test/java/ru/netology/web/page/MoneyTransfer.java
+++ src/test/java/ru/netology/web/page/MoneyTransfer.java
@@ -1,6 +1,7 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
 import ru.netology.web.data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
@@ -16,7 +17,9 @@ public class MoneyTransfer {
     private SelenideElement transferButton = $("[data-test-id=action-transfer]");
 
     public DashboardPage moneyTransfer(int amount, DataHelper.CardInfo from) {
+        $("[data-test-id=amount] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         amountField.setValue(valueOf(amount));
+        $("[data-test-id=from] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         fromField.setValue(String.valueOf(from));
         transferButton.click();
         return new DashboardPage();
